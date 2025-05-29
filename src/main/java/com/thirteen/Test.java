7921399968454567679940bc032f47ff75595763
MatthewTokdua <mtokuda0001@mymail.lausd.net>
2025-05-29 10:40:42 -0700

    Update Test.java

--- src/main/java/com/thirteen/Test.java
+++ src/main/java/com/thirteen/Test.java
@@ -6,19 +6,10 @@ public class FinchDemo {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
-        myFinch.playNote(60,0.5);
-
-        for (int i = 0; i < 4; i++) {
-            myFinch.setTail(i+1,255,0,0);
-            myFinch.setBeak(255, 100, 100);
-            myFinch.pause(1);
-            myFinch.setBeak(0, 0, 0);
-            myFinch.setTail(i+1,0,0,0);
-            myFinch.pause(1);
+        while(){
+            myFinch.getDistance()
         }
 
-        myFinch.setTurn("L",90,100);
-        myFinch.setMove("F",20,100);
 
 
 
