a5dcf398791e65b73420127fca7d2aa1755557d5
MatthewTokdua <mtokuda0001@mymail.lausd.net>
2025-05-30 09:51:17 -0700

    Update Test.java

--- src/main/java/com/thirteen/Test.java
+++ src/main/java/com/thirteen/Test.java
@@ -6,8 +6,12 @@ public class FinchDemo {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
-        while(){
-            myFinch.getDistance()
+        int flag = 0;
+
+        while(flag < 5){
+            myFinch.setMove(myFinch.getDistance()-1);
+            flag++;
+            myFinch.pause(1);
         }
 
 
