af71b5a434ba8f8b07d182e8eb2e8883450fd45c
MatthewTokdua <mtokuda0001@mymail.lausd.net>
2025-06-02 10:09:56 -0700

    Update Test.java

--- src/main/java/com/thirteen/Test.java
+++ src/main/java/com/thirteen/Test.java
@@ -6,22 +6,23 @@ public class FinchDemo {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
-        int flag = 0;
+        //int flag = 0;
+        boolean flag = true;
 
-        while(flag < 20){
-            if (myFinch.getDistance()-5 < 100 && myFinch.getDistance()-5 > 0){
-            myFinch.setMove("F",myFinch.getDistance()-5,10);
+        while(flag){ //while loop so the robot moves until it gets to the end
+            if (myFinch.getDistance() < 150 && myFinch.getDistance()-5 > 0){ //moves if the robot isn't facing a wall or open air
+            myFinch.setMove("F",myFinch.getDistance()-5,100);
             myFinch.pause(1);
             }
-            else {
-                myFinch.setTurn("R",90,10);
+            else { // algorithm for if the robot is facing a wall; turns right normally but turns left if, after it turns right, there is still a wall
+                myFinch.setTurn("R",90,100);
                 myFinch.pause(1);
                 if (myFinch.getDistance()-15 < 10){
-                    myFinch.setTurn("L",180,10);
+                    myFinch.setTurn("L",180,100);
                     myFinch.pause(1);
                 }
             }
-            flag++;
+            //flag++;
         }
 
 
